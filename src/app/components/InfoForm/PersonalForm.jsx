fbd0aa15a84e98ee2c57f1171881be94264a8f57
Leonardo Rocha <leokoby1@gmail.com>
2023-07-06 16:31:55 +0100

    feature new form page

---
+++ src/app/components/InfoForm/PersonalForm.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styles from '../../page.module.css'
+import {useForm} from 'react-hook-form'
+
+export default function PersonalForm({setState}) {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = (data) => {
+    const {name, email, phonenumber, postalcode, country} = data
+
+    if(name && email && phonenumber && postalcode && country){
+      setState(2)
+      localStorage.setItem('infoform', JSON.stringify(data))
+    }
+  }
+   
+  return (
+    <div  className={styles.containerForm}>
+      <h2>Personal Info</h2>
+      <form method='POST' onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="name">
+            Name:
+            <input {...register("name")} id='name' placeholder='Adul yan'/>
+          </label>
+          <label htmlFor="email">
+            Email:
+            <input {...register("email")} id='email' placeholder='exemple@exemple.com'/>
+          </label>
+          <label htmlFor="phone">
+            Phone number:
+            <input {...register("phonenumber")} id='phone' placeholder='+351 999 888 777'/>
+          </label>
+          <label htmlFor="country">
+            Country:
+            <input {...register("country")} id='country' placeholder='Portugal'/>
+          </label>
+          <label htmlFor="postalcode">
+            Postal code:
+            <input {...register("postalcode")} id='postalcode' placeholder='1234567'/>
+          </label>
+          <input type='submit' value={'Next Step'}/>
+        </div>
+      </form>
+    </div>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,7 @@
 "use client"
 import { useState } from 'react'
 import styles from './page.module.css'
-import InfoForm from './components/InfoForm/InfoForm'
-import Plans from './components/Plans/Plans'
-import VerifyInfo from './components/VerifyInfo/VerifyInfo'
+import PersonalForm from './components/InfoForm/PersonalForm'
 
 
 
@@ -12,34 +10,14 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <aside className={styles.containerAside}>
-        <div>
-          <span className={styles.containerStep}>
-            <p className={`${styles.bullet} ${step > 0 ? styles.bulletActive : ''}`}>1</p>
-            <span className={`${styles.stepInfo} ${step > 0 ? styles.stepActive : ''}`}>
-              <p className={styles.stepNumber}>STEP 1</p>
-              <p className={styles.stepName}>YOUR INFO</p>
-            </span>
-          </span>
-          <span className={styles.containerStep}>
-            <p className={`${styles.bullet} ${step > 1 ? styles.bulletActive : ''}`}>2</p>
-            <span className={`${styles.stepInfo} ${step > 1 ? styles.stepActive : ''}`}>
-              <p className={styles.stepNumber}>STEP 2</p>
-              <p className={styles.stepName}>SELECT A PLAN</p>
-            </span>
-          </span>
-          <span className={styles.containerStep}>
-            <p className={`${styles.bullet} ${step > 2 ? styles.bulletActive : ''}`}>3</p>
-            <span className={`${styles.stepInfo} ${step > 2 ? styles.stepActive : ''}`}>
-              <p className={styles.stepNumber}>STEP 3</p>
-              <p className={styles.stepName}>VERIFY YOUR INFORMATIONS</p>
-            </span>
-          </span>
-        </div>  
-      </aside>
-      {step <= 1 ? <InfoForm setSate={setStep} /> : null}
-      {step == 2 ? <Plans  setState={setStep}/> : null}
-      {step == 3 ? <VerifyInfo/> : null }
+      <div className={styles.mainContainer}>
+        <nav>
+          <button className={`${styles.activeButton}`}>Personal Info</button>
+          <button>Select a plan</button>
+          <button>Verify your information</button>
+        </nav>
+        <PersonalForm setState={setStep}/>
+      </div>
     </main>
   )
 }
