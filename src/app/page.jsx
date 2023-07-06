6fcbe3f1b3b67a81fdf2261cb649ea338fbb7409
Leonardo Rocha <leokoby1@gmail.com>
2023-07-06 12:47:16 +0100

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,21 +16,21 @@ export default function Home() {
         <div>
           <span className={styles.containerStep}>
             <p className={`${styles.bullet} ${step > 0 ? styles.bulletActive : ''}`}>1</p>
-            <span className={`${styles.stepInfo}`}>
+            <span className={`${styles.stepInfo} ${step > 0 ? styles.stepActive : ''}`}>
               <p className={styles.stepNumber}>STEP 1</p>
               <p className={styles.stepName}>YOUR INFO</p>
             </span>
           </span>
           <span className={styles.containerStep}>
             <p className={`${styles.bullet} ${step > 1 ? styles.bulletActive : ''}`}>2</p>
-            <span className={`${styles.stepInfo}`}>
+            <span className={`${styles.stepInfo} ${step > 1 ? styles.stepActive : ''}`}>
               <p className={styles.stepNumber}>STEP 2</p>
               <p className={styles.stepName}>SELECT A PLAN</p>
             </span>
           </span>
           <span className={styles.containerStep}>
             <p className={`${styles.bullet} ${step > 2 ? styles.bulletActive : ''}`}>3</p>
-            <span className={`${styles.stepInfo}`}>
+            <span className={`${styles.stepInfo} ${step > 2 ? styles.stepActive : ''}`}>
               <p className={styles.stepNumber}>STEP 3</p>
               <p className={styles.stepName}>VERIFY YOUR INFORMATIONS</p>
             </span>
